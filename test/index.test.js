40def20bf6ae5afb6222b31b0640d94fc37a60a6
Andre Wiggins <andrewiggins@live.com>
2021-12-10 22:29:04 -0800

    Add sample test; set type to module

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+import { test } from "uvu";
+import { is } from "uvu/assert";
+import { message } from "../dist/index.js";
+
+test("correct export", async () => {
+  is(message, "Hello World!");
+});
+
+test.run();
