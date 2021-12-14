3f58c57c3c2a1429e5b15b08573b4e8aa8516cd8
Andre Wiggins <andrewiggins@live.com>
2021-12-14 00:10:40 -0800

    Remove branch creation from test

--- test.cmd
+++ test.cmd
@@ -1,7 +1,10 @@
-git br -D test-branch
-git push --delete origin test-branch
-git br test-branch
-git push -u origin test-branch
+rem Testing branch creation
+@REM git br -D test-branch
+@REM git push --delete origin test-branch
+@REM git br test-branch
+@REM git push -u origin test-branch
+
+rem Testing tag creation
 git tag -d v0.0.1-test
 git push --delete origin v0.0.1-test
 git tag v0.0.1-test
